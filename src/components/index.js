36767703b4c81a9ef4c5381c790f101f8088a8d4
Denys Pravdyvyj <30390172+SO1T@users.noreply.github.com>
2022-09-12 17:02:49 +0300

    init project with tailwind (#1)
    Co-authored-by: dpravdyvyi <denys.pravdyvyj@nexar.app>

--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import { Plate } from './plate';
-import { Home } from './welcom';
-import { WelcomeModal } from './welcomeModal';
-import { Main } from './main';
-import { Tutorial } from './tutorial';
-import { LoginModal } from './loginModal'
-
-export { Plate, Home, WelcomeModal, Main, Tutorial, LoginModal };
