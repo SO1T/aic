436e1b392dcaab4705999b89ecefe5806183ae48
ArrrtSam <artemsamilenko19999@gmail.com>
2022-09-16 23:02:07 +0300

    Fixed menu animation

--- components/SideBar/SideBar.js
+++ components/SideBar/SideBar.js
@@ -5,7 +5,8 @@ import { logo } from '../../assets'
 import { menus } from './config.js'
 
 const styles = {
-  nav: xw`flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto`,
+  nav: xw`flex-grow md:block px-0 pb-0 py-2 fixed top-36 w-full ease-in-out duration-300 opacity-0 md:pb-0 md:overflow-y-auto bg-white md:max-w-xs md:top-40 md:opacity-100`,
+  activeMenu: xw`flex-grow md:block px-0 pb-0 py-2 fixed top-36 w-full md:pb-0 ease-in-out duration-300 opacity-100 md:overflow-y-auto md:max-w-xs md:top-40`,
   navLink: xw`flex items-center gap-3 px-4 py-2 mt-2 text-sm font-semibold text-black bg-white rounded-lg hover:bg-blue-100 focus:bg-blue-500 focus:text-white`,
   logoContainer: xw`flex-shrink-0 px-8 py-4 flex flex-row items-center justify-center`,
   logoLink: xw`text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-none`,
@@ -14,7 +15,7 @@ const styles = {
 }
 
 export const SideBar = ({}) => {
-  const [sidebarOpen, setSideBarOpen] = useState(true)
+  const [sidebarOpen, setSideBarOpen] = useState(false)
   return (
     <div css={styles.sidebarWrapper}>
       <div css={styles.logoContainer}>
@@ -28,22 +29,22 @@ export const SideBar = ({}) => {
             {sidebarOpen ? (
               <path
                 fillRule="evenodd"
-                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                 clipRule="evenodd"
               ></path>
-            ) : (
+            ): (
               <path
                 fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
                 clipRule="evenodd"
               ></path>
             )}
           </svg>
-        </button>
+        </button> 
       </div>
-      <nav css={styles.nav}>
-        {menus.map((menu) => (
-          <a css={styles.navLink} href="#">
+      <nav css={sidebarOpen ? styles.activeMenu : styles.nav}>
+        {menus.map((menu, i) => (
+          <a css={styles.navLink} key={i} href="#">
             <Image src={menu.icon} alt={'Menu icon'} width={32} height={32} />
             {menu.name}
           </a>
